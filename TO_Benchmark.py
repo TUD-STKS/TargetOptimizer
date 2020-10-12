8aadf6448a0740f789b77d32b89808026fd4d29b
PaKr1234 <paul_konstantin.krug@tu-dresden.de>
2020-10-12 18:00:04 +0200

    Updated TO_Benchmark script

--- TO_Benchmark.py
+++ TO_Benchmark.py
@@ -23,13 +23,12 @@ except Exception:
 #                     'Betriebssportgemeinschaft', 'Bratsche', 'Buch',
 #                     'Dateienunterverzeichnis','Schwarzweiss-Malerei']
 
-example_file_list = ['Analog-digital-Wandler',
-                     'Betriebssportgemeinschaft', 'Bratsche', 'Buch',
+example_file_list = ['Betriebssportgemeinschaft', 'Bratsche', 'Buch',
                      'Dateienunterverzeichnis','Schwarzweiss-Malerei']
 
 #example_bounds_list = [4,5,9,7,3,2,9,6]
 
-example_bounds_list = [9,7,3,2,9,6]
+example_bounds_list = [7,3,2,9,6]
 
 test_list = ['Analog-digital-Wandler']
 
