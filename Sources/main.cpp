e6763a310ee4c95ed253597f79f681eafed3655e
Simon Stone <simon.stone@tu-dresden.de>
2021-01-06 09:48:13 +0100

    Console output is now rerouted to a log file when the GUI is used.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <filesystem>
 #include <string>
 #include <dlib/cmd_line_parser.h>
 #include "gui.h"
@@ -17,7 +18,7 @@ public:
 	virtual bool OnInit();
 
 public:
-	MainWindow *mainWindow;
+	MainWindow* mainWindow;
 };
 
 wxIMPLEMENT_APP_NO_MAIN(TargetOptimizerGui);
@@ -33,13 +34,19 @@ bool TargetOptimizerGui::OnInit()
 
 int main(int argc, char* argv[])
 {
-	if(argc < 2)
+	if (argc < 2)
 	{
 #ifdef USE_WXWIDGETS
-			// ********** open wxWidgets GUI *****
-			auto* targetOptimizerGui = new TargetOptimizerGui();
-			wxApp::SetInstance(targetOptimizerGui);
-			wxEntry(argc, argv);
+		// If using the GUI redirect console output to log file
+		std::cout << "When using the graphical user interface, all output to stdout is re-routed to a log file 'TargetOptimizer.log'."
+			<< "Look for it in " << std::filesystem::current_path() << std::endl;
+		std::ofstream outFile("TargetOptimizer.log");
+		std::cout.rdbuf(outFile.rdbuf());
+
+		// ********** open wxWidgets GUI *****
+		auto* targetOptimizerGui = new TargetOptimizerGui();
+		wxApp::SetInstance(targetOptimizerGui);
+		wxEntry(argc, argv);
 
 #else
 		std::cerr << "TargetOptimizer was compiled without GUI support.\n"
@@ -55,37 +62,37 @@ int main(int argc, char* argv[])
 			dlib::command_line_parser parser;
 
 			// command line options
-			parser.add_option("h","Display this help message.");
+			parser.add_option("h", "Display this help message.");
 			parser.set_group_name("Output Options");
-			parser.add_option("g","Choose for VTL gesture file.");
-			parser.add_option("c","Choose for csv table file.");
-			parser.add_option("p","Choose for PitchTier file.");
+			parser.add_option("g", "Choose for VTL gesture file.");
+			parser.add_option("c", "Choose for csv table file.");
+			parser.add_option("p", "Choose for PitchTier file.");
 			parser.set_group_name("Additional Parameter Options");
-			parser.add_option("tg","Specify a TextGrid file.",1);
-			parser.add_option("lambda","Specify regularization parameter.",1);
-			parser.add_option("m-range","Specify search space for slope parameter.",1);
-			parser.add_option("b-range","Specify search space for offset parameter.",1);
-			parser.add_option("t-range","Specify search space for time constant parameter.",1);
-			parser.add_option("m-weight","Specify regularization weight for slope parameter.",1);
-			parser.add_option("b-weight","Specify regularization weight for offset parameter.",1);
-			parser.add_option("t-weight","Specify regularization weight for time constant parameter.",1);
-			parser.add_option("boundaryDelta","Specify boundary delta.",1);
-			parser.add_option("initBounds","Specify how many bounds should be initialized.",1);
-			parser.add_option("maxIterations","Specify maximum number of optimizer iterations.",1);
-			parser.add_option("maxCostEvaluations","Specify maximum number of costfunction evaluations.",1);
-			parser.add_option("rhoEnd","Specify the rho_end parameter.",1);
+			parser.add_option("tg", "Specify a TextGrid file.", 1);
+			parser.add_option("lambda", "Specify regularization parameter.", 1);
+			parser.add_option("m-range", "Specify search space for slope parameter.", 1);
+			parser.add_option("b-range", "Specify search space for offset parameter.", 1);
+			parser.add_option("t-range", "Specify search space for time constant parameter.", 1);
+			parser.add_option("m-weight", "Specify regularization weight for slope parameter.", 1);
+			parser.add_option("b-weight", "Specify regularization weight for offset parameter.", 1);
+			parser.add_option("t-weight", "Specify regularization weight for time constant parameter.", 1);
+			parser.add_option("boundaryDelta", "Specify boundary delta.", 1);
+			parser.add_option("initBounds", "Specify how many bounds should be initialized.", 1);
+			parser.add_option("maxIterations", "Specify maximum number of optimizer iterations.", 1);
+			parser.add_option("maxCostEvaluations", "Specify maximum number of costfunction evaluations.", 1);
+			parser.add_option("rhoEnd", "Specify the rho_end parameter.", 1);
 			parser.add_option("epsilon", "Specify the epsilon for early stopping", 1);
 			parser.add_option("patience", "Specify the patience for early stopping", 1);
 			parser.add_option("log", "Outputs f0, targets, boundaries, etc. to a single text file.");
 			parser.add_option("useEarlyStopping", "Stop the search early if fmin-ftmp < epsilon for x times (x=patience).");
-			parser.add_option("utterance","Name of the utterance to optimize.",1);
+			parser.add_option("utterance", "Name of the utterance to optimize.", 1);
 
 
 			// parse command line
-			parser.parse(argc,argv);
+			parser.parse(argc, argv);
 
 			// check command line options
-			const char* one_time_opts[] = {"h", "g", "c", "p", "m-range", "b-range", "t-range", "m-weight", "b-weight", "t-weight"};
+			const char* one_time_opts[] = { "h", "g", "c", "p", "m-range", "b-range", "t-range", "m-weight", "b-weight", "t-weight" };
 			parser.check_one_time_options(one_time_opts);
 			parser.check_option_arg_range("m-range", 0.0, 100.0);
 			parser.check_option_arg_range("b-range", 0.0, 100.0);
@@ -95,7 +102,7 @@ int main(int argc, char* argv[])
 			parser.check_option_arg_range("t-weight", 0.0, 1e9);
 			parser.check_option_arg_range("lambda", 0.0, 1e15);
 			parser.check_option_arg_range("boundaryDelta", 0.0, 100.0);
-			parser.check_option_arg_range("maxIterations",1,20000);
+			parser.check_option_arg_range("maxIterations", 1, 20000);
 
 			// process help option
 			if (parser.option("h"))
@@ -118,12 +125,13 @@ int main(int argc, char* argv[])
 			parameters.searchSpaceParameters.initBounds = get_option(parser, "initBounds", 0);
 
 			BoundaryVector bounds;
-			std::string TextGrid = get_option(parser,"tg","");
-			if ( TextGrid != "")
+			std::string TextGrid = get_option(parser, "tg", "");
+			if (TextGrid != "")
 			{
 				// process TextGrid input
-				bounds = DataIO::readTextGridFile( TextGrid ).getBounds();
-			}else if ( parameters.searchSpaceParameters.initBounds < 2 )
+				bounds = DataIO::readTextGridFile(TextGrid).getBounds();
+			}
+			else if (parameters.searchSpaceParameters.initBounds < 2)
 			{
 				std::cout << "You need at least two boundaries! Specify a TextGrid file or set initBounds >=2." << std::endl;
 				return EXIT_FAILURE;
@@ -137,7 +145,7 @@ int main(int argc, char* argv[])
 
 
 			std::string LOG_PATH = parser[1];
-			std::string UTTERANCE = get_option(parser,"utterance","default_str");
+			std::string UTTERANCE = get_option(parser, "utterance", "default_str");
 
 			std::cout << "log path: " << LOG_PATH << std::endl;
 
@@ -151,46 +159,47 @@ int main(int argc, char* argv[])
 			meanF0 /= f0.size();
 
 
-			
 
-			parameters.regularizationParameters.weightSlope = get_option(parser,"m-weight",1.0);
-			parameters.regularizationParameters.weightOffset = get_option(parser,"b-weight",0.5);
-			parameters.regularizationParameters.weightTau = get_option(parser,"t-weight",0.1);
-			parameters.regularizationParameters.lambda = get_option(parser,"lambda",0.01);
 
-			parameters.searchSpaceParameters.deltaSlope = get_option(parser,"m-range",50.0);
-			parameters.searchSpaceParameters.deltaOffset = get_option(parser,"b-range",20.0);
-			parameters.searchSpaceParameters.deltaTau = get_option(parser,"t-range",5.0);
+			parameters.regularizationParameters.weightSlope = get_option(parser, "m-weight", 1.0);
+			parameters.regularizationParameters.weightOffset = get_option(parser, "b-weight", 0.5);
+			parameters.regularizationParameters.weightTau = get_option(parser, "t-weight", 0.1);
+			parameters.regularizationParameters.lambda = get_option(parser, "lambda", 0.01);
+
+			parameters.searchSpaceParameters.deltaSlope = get_option(parser, "m-range", 50.0);
+			parameters.searchSpaceParameters.deltaOffset = get_option(parser, "b-range", 20.0);
+			parameters.searchSpaceParameters.deltaTau = get_option(parser, "t-range", 5.0);
 			parameters.searchSpaceParameters.meanSlope = 0.0;
 			parameters.searchSpaceParameters.meanOffset = meanF0;
 			parameters.searchSpaceParameters.meanTau = 20.0;
 			parameters.searchSpaceParameters.deltaBoundary = get_option(parser, "boundaryDelta", 100.0);
-			
-			parameters.searchSpaceParameters.optimizeBoundaries = ( parameters.searchSpaceParameters.deltaBoundary != 0 );
-			parameters.searchSpaceParameters.numberOptVar = ( parameters.searchSpaceParameters.optimizeBoundaries ? 4 : 3 );
+
+			parameters.searchSpaceParameters.optimizeBoundaries = (parameters.searchSpaceParameters.deltaBoundary != 0);
+			parameters.searchSpaceParameters.numberOptVar = (parameters.searchSpaceParameters.optimizeBoundaries ? 4 : 3);
 
 			OptimizerOptions optOpt;
-			optOpt.maxIterations = get_option(parser,"maxIterations",50.0);
-			if ( parser.option("useEarlyStopping") )
+			optOpt.maxIterations = get_option(parser, "maxIterations", 50.0);
+			if (parser.option("useEarlyStopping"))
 			{
 				optOpt.useEarlyStopping = true;
-			}else{
+			}
+			else {
 				optOpt.useEarlyStopping = false;
 			}
-			optOpt.epsilon = get_option(parser,"epsilon", 0.01);
-			optOpt.patience = get_option(parser,"patience", 10);
-			optOpt.maxCostEvaluations = get_option(parser,"maxCostEvaluations", 1e6);
+			optOpt.epsilon = get_option(parser, "epsilon", 0.01);
+			optOpt.patience = get_option(parser, "patience", 10);
+			optOpt.maxCostEvaluations = get_option(parser, "maxCostEvaluations", 1e6);
 			optOpt.rhoEnd = get_option(parser, "rhoEnd", 1e-6);
 
 
 
 
 
-			if ( parameters.searchSpaceParameters.initBounds != 0 )
+			if (parameters.searchSpaceParameters.initBounds != 0)
 			{
 				std::cout << "bounds init" << std::endl;
 				double pitch_start = f0.at(0).time;
-				double pitch_end   = f0.back().time;
+				double pitch_end = f0.back().time;
 				double pitch_interval = pitch_end - pitch_start;
 				double step = pitch_interval / (parameters.searchSpaceParameters.initBounds - 1);
 
@@ -198,14 +207,14 @@ int main(int argc, char* argv[])
 				std::vector<double> initBoundaries;
 				for (int i = 0; i < parameters.searchSpaceParameters.initBounds; ++i)
 				{
-					initBoundaries.push_back( pitch_start + i * step );
+					initBoundaries.push_back(pitch_start + i * step);
 				}
 				bounds = initBoundaries;
 				initBoundaries.clear();
 			}
 
 			// main functionality
-			OptimizationProblem problem (parameters, f0, bounds);
+			OptimizationProblem problem(parameters, f0, bounds);
 			BobyqaOptimizer optimizer;
 			optimizer.optimize(problem, optOpt);
 			TargetVector optTargets = problem.getPitchTargets();
@@ -214,10 +223,10 @@ int main(int argc, char* argv[])
 			Sample onset = problem.getOnset();
 			std::vector<double> ftmp_vector = problem.getOptimizationSolutions();
 
-			if ( parser.option("log") )
+			if (parser.option("log"))
 			{
 				std::ofstream LOG;
-				LOG.open ( LOG_PATH );
+				LOG.open(LOG_PATH);
 				LOG << "utterance, ";
 				LOG << "par_s_slope_delta, ";
 				LOG << "par_s_offset_delta, ";
@@ -263,7 +272,7 @@ int main(int argc, char* argv[])
 				LOG << parameters.regularizationParameters.weightSlope << ", ";
 				LOG << parameters.regularizationParameters.weightOffset << ", ";
 				LOG << parameters.regularizationParameters.weightTau << ", ";
-				LOG << optOpt.maxIterations<< ", ";
+				LOG << optOpt.maxIterations << ", ";
 				LOG << optOpt.maxCostEvaluations << ", ";
 				LOG << optOpt.rhoEnd << ", ";
 				LOG << optOpt.useEarlyStopping << ", ";
@@ -281,7 +290,7 @@ int main(int argc, char* argv[])
 				LOG << ", ";
 				for (unsigned i = 0; i < bounds.size(); ++i)
 				{
-					LOG << bounds.at(i)<< " ";
+					LOG << bounds.at(i) << " ";
 				}
 				LOG << ", ";
 				LOG << problem.getCostFunction() << ", ";
@@ -305,7 +314,7 @@ int main(int argc, char* argv[])
 				LOG << ", ";
 				for (unsigned i = 0; i < optBoundaries.size(); ++i)
 				{
-					LOG << optBoundaries.at(i)<< " ";
+					LOG << optBoundaries.at(i) << " ";
 				}
 				LOG << ", ";
 				for (unsigned i = 0; i < optTargets.size(); ++i)
@@ -331,26 +340,26 @@ int main(int argc, char* argv[])
 
 
 
-//				for (unsigned i = 0; i < f0.size(); ++i)
-//				{
-//					LOG_F0 << "ORI_F0 " << f0.at(i).time << " " << f0.at(i).value << "\n";
-//				}
-//				for (unsigned i = 0; i < bounds.size(); ++i)
-//				{
-//					LOG_F0 << "ORI_BOUNDS " << bounds.at(i) <<  "\n";
-//				}
-//				for (unsigned i = 0; i < optF0.size(); ++i)
-//				{
-//					LOG_F0 << "OPT_F0 " << optF0.at(i).time << " " << optF0.at(i).value << "\n";
-//				}
-//				for (unsigned i = 0; i < optTargets.size(); ++i)
-//				{
-//					LOG_F0 << "OPT_TARGETS " << optTargets.at(i).slope << " " << optTargets.at(i).offset << " " << optTargets.at(i).tau << " " << optTargets.at(i).duration << " " <<  "\n";
-//				}
-//				for (unsigned i = 0; i < optBoundaries.size(); ++i)
-//				{
-//					LOG_F0 << "OPT_BOUNDS " << optBoundaries.at(i) <<  "\n";
-//				}
+				//				for (unsigned i = 0; i < f0.size(); ++i)
+				//				{
+				//					LOG_F0 << "ORI_F0 " << f0.at(i).time << " " << f0.at(i).value << "\n";
+				//				}
+				//				for (unsigned i = 0; i < bounds.size(); ++i)
+				//				{
+				//					LOG_F0 << "ORI_BOUNDS " << bounds.at(i) <<  "\n";
+				//				}
+				//				for (unsigned i = 0; i < optF0.size(); ++i)
+				//				{
+				//					LOG_F0 << "OPT_F0 " << optF0.at(i).time << " " << optF0.at(i).value << "\n";
+				//				}
+				//				for (unsigned i = 0; i < optTargets.size(); ++i)
+				//				{
+				//					LOG_F0 << "OPT_TARGETS " << optTargets.at(i).slope << " " << optTargets.at(i).offset << " " << optTargets.at(i).tau << " " << optTargets.at(i).duration << " " <<  "\n";
+				//				}
+				//				for (unsigned i = 0; i < optBoundaries.size(); ++i)
+				//				{
+				//					LOG_F0 << "OPT_BOUNDS " << optBoundaries.at(i) <<  "\n";
+				//				}
 				LOG.close();
 			}
 
@@ -378,7 +387,7 @@ int main(int argc, char* argv[])
 			return EXIT_SUCCESS;
 		}
 
-		catch (std::exception& e)
+		catch (std::exception & e)
 		{
 			std::cerr << "[main] Program was terminated because an error occurred!\n" << e.what() << std::endl;
 			return EXIT_FAILURE;
