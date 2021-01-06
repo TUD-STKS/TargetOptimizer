7299bb916db4e3b72dfe10f7e97f83ed3d94ddd4
Simon Stone <simon.stone@tu-dresden.de>
2021-01-06 17:20:55 +0100

    Added code to make the calls to the std::filesystem functions portable to GCC.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -10,7 +10,14 @@
 
 #ifdef USE_WXWIDGETS
 #include <wx/wx.h>
+
+#if defined(__GNUC__)
+// The GCC considers the C++17 filesystem header experimental
 #include <experimental/filesystem>
+namespace std::filesystem = std::experimental::filesystem
+#else
+#include <filesystem>
+#endif
 
 class TargetOptimizerGui : public wxApp
 {
@@ -39,7 +46,7 @@ int main(int argc, char* argv[])
 #ifdef USE_WXWIDGETS
 		// If using the GUI redirect console output to log file
 		std::cout << "When using the graphical user interface, all output to stdout is re-routed to a log file 'TargetOptimizer.log'."
-			<< "Look for it in " << std::experimental::filesystem::current_path().string() << std::endl;
+			<< "Look for it in " << std::filesystem::current_path().string() << std::endl;
 		std::ofstream outFile("TargetOptimizer.log");
 		std::cout.rdbuf(outFile.rdbuf());
 
